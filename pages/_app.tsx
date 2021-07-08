a942f71446f645dd2bed6f4d881f67e743d43bbf
Air2air <todd@dunningmarketing.com>
2021-07-07 17:13:11 -0700

    Apollo tutorial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
-export default MyApp
+export default MyApp;
+
+
---
+++ pages/client-side.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ClientOnly from "../components/ClientOnly";
+import Countries from "../components/Countries";
+
+export default function ClientSide() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <ClientOnly>
+          <Countries />
+        </ClientOnly>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}

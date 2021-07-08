13ef4b2fae2827888aa7e1d71a3716c607d36606
Air2air <todd@dunningmarketing.com>
2021-07-07 21:04:04 -0700

    Api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 import "../styles/globals.scss";
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
@@ -11,5 +10,3 @@ function MyApp({ Component, pageProps }) {
   );
 }
 export default MyApp;
-
-

d2be874bea8059a97319c8db6599b6dedbcf3367
Air2air <todd@dunningmarketing.com>
2021-07-21 15:41:53 -0700

    New favicons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,60 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
-import { motion } from "framer-motion";
-import logo from "./../../images/jalapeno.svg";
-import logoBlack from "./../../images/jalapeno-black.svg";
+// import { motion } from "framer-motion";
+// import logo from "./../../images/jalapeno.svg";
+// import logoBlack from "./../../images/jalapeno-black.svg";
 import "./styles.scss";
 
 const Header = () => {
-  const pepperMotion = {
-    rest: { opacity: 1, scale: 1 },
-    hover: {
-      scale: 1.2,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
+  // const pepperMotion = {
+  //   rest: { opacity: 1, scale: 1 },
+  //   hover: {
+  //     scale: 1.2,
+  //     transition: {
+  //       duration: 0.2,
+  //     },
+  //   },
+  // };
 
-  const shadowMotion = {
-    rest: { scale: 1 },
-    hover: {
-      scale: 0.6,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
+  // const shadowMotion = {
+  //   rest: { scale: 1 },
+  //   hover: {
+  //     scale: 0.6,
+  //     transition: {
+  //       duration: 0.2,
+  //     },
+  //   },
+  // };
+  // const pepperHeader = {
+  //   return (
+  //     <Navbar
+  //       expand="md"
+  //       className="navbar navbar-expand-lg mb-5 navbar-dark navbar-fixed"
+  //       fixed="top"
+  //     >
+  //       <Container fluid className="d-flex justify-content-center">
+  //         <Nav.Link
+  //           href="mailto:contact@eljefeventures.com?subject=Inquiry for El Jefe"
+  //           role="button"
+  //         >
+  //           <motion.div
+  //             className="blur-box"
+  //             initial="rest"
+  //             whileHover="hover"
+  //             animate="rest"
+  //           >
+  //             <motion.div variants={shadowMotion} className="header-logo-blur">
+  //               <img src={logoBlack} alt="El Jefe" />
+  //             </motion.div>
+  //             <motion.div variants={pepperMotion} className="header-logo">
+  //               <img src={logo} alt="El Jefe" />
+  //             </motion.div>
+  //           </motion.div>
+  //         </Nav.Link>
+  //       </Container>
+  //     </Navbar>
+  //   );
+  // };
 
   return (
     <Navbar
@@ -37,19 +67,7 @@ const Header = () => {
           href="mailto:contact@eljefeventures.com?subject=Inquiry for El Jefe"
           role="button"
         >
-          <motion.div
-            className="blur-box"
-            initial="rest"
-            whileHover="hover"
-            animate="rest"
-          >
-            <motion.div variants={shadowMotion} className="header-logo-blur">
-              <img src={logoBlack} alt="El Jefe" />
-            </motion.div>
-            <motion.div variants={pepperMotion} className="header-logo">
-              <img src={logo} alt="El Jefe" />
-            </motion.div>
-          </motion.div>
+          <span class="navbar-toggler-icon"></span>
         </Nav.Link>
       </Container>
     </Navbar>

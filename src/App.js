5ee62aed337235622d08a504b8bf698c333fc7ac
Air2air <todd@dunningmarketing.com>
2021-07-26 21:18:33 -0700

    Charts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { HeaderLoggedIn, HeaderLoggedOut } from "./components/Header";
 import Home from "./pages/Home";
 import Service from "./pages/Service";
 
 const App = () => {
 
-  const isLoggedIn = true;
+  
+  const isLoggedIn = false;
+
   return (
     <Router>
-      <Header isLoggedin={isLoggedIn} />
+      {isLoggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
       <Switch>
         <Route exact path={["/", "/home"]} component={Home} />
         <Route path={"/service"} component={Service} />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,27 @@
 import React from "react";
-import { Nav, Navbar, Container, NavbarBrand } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 import logo from "./../../images/jalapeno.svg";
 import "./logged-in.scss";
 import "./logged-out.scss";
 
-const Header = (props) => {
-  const HeaderLoggedIn = () => (
+const HeaderLoggedIn = () => {
+  return (
     <Navbar
       expand="md"
       className="navbar navbar-expand-lg mb-5 navbar-dark navbar-fixed logged-in"
       fixed="top"
     >
-      <Container fluid className="d-flex justify-content-between">
-
-        <NavbarBrand>
+      <Container fluid className="d-flex justify-content-center">
+        <Nav.Link href="mailto:contact@eljefeventures.com?subject=Inquiry for El Jefe">
           <img src={logo} className="header-logo" alt="El Jefe" />
-        </NavbarBrand>
-
-        <Nav.Link
-          href="mailto:contact@eljefeventures.com?subject=Inquiry for El Jefe"
-          role="button"
-        >
-          button
-        </Nav.Link>
-
-        <Nav.Link
-          href="mailto:contact@eljefeventures.com?subject=Inquiry for El Jefe"
-          role="button"
-        >
-          Log out
         </Nav.Link>
       </Container>
     </Navbar>
   );
+};
 
-  const HeaderLoggedOut = () => (
+const HeaderLoggedOut = () => {
+  return (
     <Navbar
       expand="md"
       className="navbar navbar-expand-lg mb-5 navbar-dark navbar-fixed logged-out"
@@ -50,10 +37,6 @@ const Header = (props) => {
       </Container>
     </Navbar>
   );
-
-  return (
-    <div>{props.isLoggedin ? <HeaderLoggedIn /> : <HeaderLoggedOut />}</div>
-  );
 };
 
-export default Header;
+export { HeaderLoggedIn, HeaderLoggedOut };

79b9213528b865a04f3dc5736a3ff0d7384ec010
Air2air <todd@dunningmarketing.com>
2021-07-28 16:06:33 -0700

    Layouts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Service from "./pages/Service";
 
 const App = () => {
-
 
   return (
     <Router>
       <Header />
       <Switch>
         <Route exact path={["/", "/home"]} component={Home} />
-        <Route exact path={"/service"} component={Service} />
+        <Route path={"/service"} component={Service} />
       </Switch>
-      <Footer />
     </Router>
   );
 };

64254b6707130eb62ab09ead95632837d6598af5
Air2air <todd@dunningmarketing.com>
2021-07-27 17:39:10 -0700

    Chart state

--- src/components/ChartBar/index.js
+++ src/components/ChartBar/index.js
@@ -1,101 +1,25 @@
-import React from "react";
-import { ResponsiveBar } from "@nivo/bar";
+import React, { useState } from "react";
+import ChartBar from "./chart";
+import { bardata1, bardata2 } from "./../../data/bardata.js";
 import "./styles.scss";
-import text from "./../../styles/text.scss";
-import colors from "./../../styles/variables.scss";
 
-const axisBottom = {
-  tickSize: 5,
-  tickPadding: 5,
-  tickRotation: 0,
-  legend: "Map",
-  legendPosition: "middle",
-  legendOffset: 32,
+const ChartBarComponent = (props) => {
+  const [toggle, setToggle] = useState(0);
+
+  return (
+    <>
+      <div className="chart-component-wrapper">
+        <div className="chart-header d-flex align-items-center justify-content-between">
+          <h2>{props.title}</h2>
+          <div>
+            <button onClick={() => setToggle(1)}>Toggle State</button>
+            <button onClick={() => setToggle(0)}>Toggle State</button>
+          </div>
+        </div>
+        <ChartBar dataSource={toggle === 1 ? bardata1 : bardata2} />
+      </div>
+    </>
+  );
 };
 
-const axisLeft = {
-  tickSize: 5,
-  tickPadding: 5,
-  tickRotation: 0,
-  legend: "Wins / Loss",
-  legendPosition: "middle",
-  legendOffset: -40,
-};
-
-const theme = {
-  background: "none",
-  axis: {
-    tickColor: "#eee",
-    ticks: {
-      line: {
-        stroke: "#555555",
-      },
-      text: {
-        fontSize: text.fontSize,
-        fill: "#ffffff",
-      },
-    },
-    legend: {
-      text: {
-        fill: "#aaaaaa",
-      },
-    },
-  },
-  grid: {
-    line: {
-      stroke: "#555555",
-    },
-  },
-};
-
-const colorBy = ({ id }) => (id === "me" ? colors.blue : colors.gold);
-
-const legends = [
-  {
-    dataFrom: "keys",
-    anchor: "bottom-right",
-    direction: "column",
-    justify: false,
-    translateX: 120,
-    translateY: 0,
-    itemsSpacing: 2,
-    itemWidth: 100,
-    itemHeight: 20,
-    itemDirection: "left-to-right",
-    itemOpacity: 0.85,
-    itemTextColor: "#ffffff",
-    symbolSize: 20,
-    effects: [
-      {
-        on: "hover",
-        style: {
-          itemOpacity: 1,
-        },
-      },
-    ],
-  },
-];
-
-const ChartBar = (props) => (
-  <div className="chart-bar-wrapper">
-    <ResponsiveBar
-      margin={{ top: 60, right: 120, bottom: 60, left: 80 }}
-      data={props.dataSource}
-      keys={["me", "benchmark"]}
-      indexBy="map"
-      labelTextColor="inherit:darker(2.4)"
-      labelSkipWidth={12}
-      labelSkipHeight={12}
-      enableGridX={false}
-      axisBottom={axisBottom}
-      axisLeft={axisLeft}
-      colors={colorBy}
-      theme={theme}
-      legends={legends}
-      animate={true}
-      groupMode="grouped"
-    />
-  </div>
-);
-
-export default ChartBar;
+export default ChartBarComponent;

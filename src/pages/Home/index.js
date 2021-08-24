0cebf8afdfd51645d0a7876a2d8e35e1966c6699
Air2air <todd@dunningmarketing.com>
2021-08-23 21:29:21 -0700

    Homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Header from "./../../components/Header";
+import logo from "./../../images/text_logo.svg";
+import { motion } from "framer-motion";
 import "./styles.scss";
 
 const Home = () => {
   return (
     <>
-      <Header authState="LoggedOut" />
-      <div className="loading">
-        <div className="main"></div>
+      <div className="full-screen">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 4 }}
+          className="logo-div"
+        >
+          <img className="logo" src={logo} alt="El Jefe Ventures" />
+        </motion.div>
       </div>
     </>
   );

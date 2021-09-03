6d2b75e89358b4b7fe3ace8072277a9a285021a1
Air2air <todd@dunningmarketing.com>
2021-09-03 15:27:25 -0700

    Works with toast

--- src/components/Portfolio/FundAdd.js
+++ src/components/Portfolio/FundAdd.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import {
   Box,
   Button,
-  Center,
   Collapse,
   Flex,
   FormControl,
@@ -11,13 +10,14 @@ import {
   Input,
   Spacer,
   Text,
+  useToast,
   useDisclosure,
 } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa";
 import ElJefeAPI from "../../api/elJefeApi";
 
-const AddFund = props => {
-  const portfolioNewState = {
+const AddFund = () => {
+  const blankFundState = {
     id: null,
     fundName: "",
     fundValue: "",
@@ -25,33 +25,38 @@ const AddFund = props => {
   };
 
   const { isOpen, onToggle } = useDisclosure();
-  const [portfolio, setPortfolio] = useState(portfolioNewState);
-  const [submitted, setSubmitted] = useState(false);
+  const [fund, setNewFund] = useState(blankFundState);
+  const toast = useToast();
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setPortfolio({ ...portfolio, [name]: value });
+    setNewFund({ ...fund, [name]: value });
   };
 
-  const savePortfolio = () => {
+  const saveNewFund = () => {
     var data = {
-      id: portfolio.id,
-      fundName: portfolio.fundName,
-      fundValue: portfolio.fundValue,
-      fundBasis: portfolio.fundBasis,
+      id: fund.id,
+      fundName: fund.fundName,
+      fundValue: fund.fundValue,
+      fundBasis: fund.fundBasis,
     };
 
     ElJefeAPI.create(data)
       .then((response) => {
-        setPortfolio({
+        setNewFund({
           id: response.data.id,
           fundName: response.data.fundName,
           fundValue: response.data.fundValue,
           fundBasis: response.data.fundBasis,
         });
-        setSubmitted(true);
-        onToggle(false);
         console.log(response.data);
+        toast({
+          title: "Saved!",
+          description: "You've added " + fund.fundName + ".",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
       })
       .catch((e) => {
         console.log(e);
@@ -68,72 +73,64 @@ const AddFund = props => {
         </Flex>
         <Collapse in={isOpen}>
           <Box p={3} pt={0}>
-            {submitted ? (
-              <Center h="90px">
-                <h4>
-                  You successfully added {portfolio.fundName} to your portfolio.
-                </h4>
-              </Center>
-            ) : (
-              <form>
-                <Flex h="90px">
-                  <FormControl id="fundName" mr={4}>
-                    <FormLabel>Name</FormLabel>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      id="fundName"
-                      required
-                      value={portfolio.fundName}
-                      onChange={handleInputChange}
-                      name="fundName"
-                      color="gray.600"
-                      bg="gray.100"
-                      _hover={{ color: "gray.900", bg: "white" }}
-                    />
-                  </FormControl>
+            <form>
+              <Flex h="90px">
+                <FormControl id="fundName" mr={4}>
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    id="fundName"
+                    required
+                    value={fund.fundName}
+                    onChange={handleInputChange}
+                    name="fundName"
+                    color="gray.600"
+                    bg="gray.100"
+                    _hover={{ color: "gray.900", bg: "white" }}
+                  />
+                </FormControl>
 
-                  <FormControl id="fundBasis" mx={4}>
-                    <FormLabel>$ Basis</FormLabel>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      id="fundBasis"
-                      required
-                      value={portfolio.fundBasis}
-                      onChange={handleInputChange}
-                      name="fundBasis"
-                      color="gray.600"
-                      bg="gray.100"
-                      _hover={{ color: "gray.900", bg: "white" }}
-                    />
-                  </FormControl>
+                <FormControl id="fundBasis" mx={4}>
+                  <FormLabel>$ Basis</FormLabel>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    id="fundBasis"
+                    required
+                    value={fund.fundBasis}
+                    onChange={handleInputChange}
+                    name="fundBasis"
+                    color="gray.600"
+                    bg="gray.100"
+                    _hover={{ color: "gray.900", bg: "white" }}
+                  />
+                </FormControl>
 
-                  <FormControl id="fundValue" ml={4}>
-                    <FormLabel>$ Value</FormLabel>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      id="fundValue"
-                      value={portfolio.fundValue}
-                      onChange={handleInputChange}
-                      name="fundValue"
-                      color="gray.600"
-                      bg="gray.100"
-                      _hover={{ color: "gray.900", bg: "white" }}
-                    />
-                  </FormControl>
-                </Flex>
-                <Flex justifyContent="space-between">
-                  <Button colorScheme="red" onClick={onToggle}>
-                    Cancel
-                  </Button>
-                  <Button colorScheme="green" onClick={savePortfolio}>
-                    Add Fund
-                  </Button>
-                </Flex>
-              </form>
-            )}
+                <FormControl id="fundValue" ml={4}>
+                  <FormLabel>$ Value</FormLabel>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    id="fundValue"
+                    value={fund.fundValue}
+                    onChange={handleInputChange}
+                    name="fundValue"
+                    color="gray.600"
+                    bg="gray.100"
+                    _hover={{ color: "gray.900", bg: "white" }}
+                  />
+                </FormControl>
+              </Flex>
+              <Flex justifyContent="space-between">
+                <Button colorScheme="red" onClick={onToggle}>
+                  Cancel
+                </Button>
+                <Button colorScheme="green" onClick={saveNewFund}>
+                  Add Fund
+                </Button>
+              </Flex>
+            </form>
           </Box>
         </Collapse>
       </Box>
